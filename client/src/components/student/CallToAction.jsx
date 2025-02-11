494e201c4b1971460410a2b4d520659c12ab721f
aadilparmar <124196983+aadilparmar@users.noreply.github.com>
2025-02-12 01:11:51 +0530

    sixth-commit

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import { assets } from "../../assets/assets";
 
 const CallToAction = () => {
+  
   return (
     <div className="flex flex-col items-center gap-4 pt-10 pb-24 px-8 md:px-0">
-      <h1 className="text-xl md:text-4xl text-gray-800 font-semibold ">Learn anything , anytime , anywhere</h1>
+      <h1 className="text-xl md:text-4xl text-gray-800 font-semibold ">
+        Learn anything , anytime , anywhere
+      </h1>
       <p className="text-gray-500 sm:text-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam officiis
-        laudantium delectus asperiores,<br />at saepe aperiam voluptatum iusto omnis
-        iure rerum, voluptatem quidem quasi voluptate est distinctio soluta
-        debitis aliquid?
+        At QT-WORLD we aim to create a global community that believes in
+        friendly competition and consistent learning. <br /> QT-WORLD works on the
+        principle of providing everyone with easy to share strategies,<br /> and
+        useful tips in the form of tutorials, resources, etc.
       </p>
       <div className="flex items-center font-medium gap-6 mt-5">
-        <button className="px-10 py-3 rounded-md text-white bg-blue-600">Get Started</button>
-        <button className="flex items-center gap-2">Learn More <img src={assets.arrow_icon} alt="arrow_icon" /> </button>
+        <button className="px-10 py-3 rounded-md text-white bg-blue-600">
+          Get Started
+        </button>
+        <button className="flex items-center gap-2">
+          Learn More <img src={assets.arrow_icon} alt="arrow_icon" />{" "}
+        </button>
       </div>
-    </div>  
+    </div>
   );
 };
 
--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -3,10 +3,16 @@ import { useParams } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/student/Loading";
 import { assets } from "../../assets/assets";
+import humanizeDuration from "humanize-duration";
+import Footer from "../../components/student/Footer";
 const CourseDetails = () => {
   const { id } = useParams();
   const [courseData, setCourseData] = useState(null);
+  const [openSection, setOpenSection] = useState({});
+  const [isAllreadyEnrolled, setIsAllReadyEnrolled] = useState(true);
+
   const {
+    currency,
     allCourses,
     calculateRating,
     calculateChapterTime,
@@ -20,9 +26,15 @@ const CourseDetails = () => {
   useEffect(() => {
     fetchCourseData();
   });
+  const toggleSection = (index) => {
+    setOpenSection((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
   return courseData ? (
     <>
-      <div className="flex md:flex -row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
         <div className="absolute left-0 top-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-300/70"></div>
 
         {/* left Column */}
@@ -30,10 +42,11 @@ const CourseDetails = () => {
           <h1 className="md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800">
             {courseData.courseTitle}
           </h1>
+          <div></div>
           <p
             className="pt-4 md:text-base text-sm"
             dangerouslySetInnerHTML={{
-              __html: courseData.courseDescription.slice(0, 300),
+              __html: courseData.courseDescription.slice(0, 200),
             }}
           ></p>
           {/* Reviews and ratings */}
@@ -71,9 +84,18 @@ const CourseDetails = () => {
                   key={index}
                   className="border border-gray-300 bg-white mb-2 rounded"
                 >
-                  <div className="flex items-center justify-between px-4 py-3 cursor-pointer select-none">
+                  <div
+                    className="flex items-center justify-between px-4 py-3 cursor-pointer select-none"
+                    onClick={() => toggleSection(index)}
+                  >
                     <div className="flex items-center gap-2">
-                      <img src={assets.down_arrow_icon} alt="arrow_icons" />
+                      <img
+                        src={assets.down_arrow_icon}
+                        alt="arrow_icons"
+                        className={`transform transition-transform ${
+                          openSection[index] ? "rotate-180" : ""
+                        }`}
+                      />
                       <p className="font-medium md:text-base text-sm">
                         {chapter.chapterTitle}
                       </p>
@@ -83,14 +105,125 @@ const CourseDetails = () => {
                       {calculateChapterTime(chapter)}
                     </p>
                   </div>
+                  <div
+                    className={`overflow-hidden transition-all duration-3000 ${
+                      openSection[index] ? "max-h-96" : "max-h-0"
+                    }`}
+                  >
+                    <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
+                      {chapter.chapterContent.map((lecture, i) => (
+                        <li className="flex items-start gap-2 py-1" key={i}>
+                          <img
+                            src={assets.play_icon}
+                            alt="play icon"
+                            className="w-4 h-4 mt-1"
+                          />
+                          <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
+                            <p>{lecture.lectureTitle}</p>
+                            <div className="flex gap-2">
+                              {lecture.isPreviewFree && (
+                                <p className="text-blue-500 cursor-pointer">
+                                  Preview
+                                </p>
+                              )}
+                              <p>
+                                {humanizeDuration(
+                                  lecture.lectureDuration * 60 * 1000,
+                                  { units: ["h", "m"] }
+                                )}
+                              </p>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
               ))}
             </div>
           </div>
+          <div className="text-sm md:text-default py-20">
+            <h3 className="text-xl font-semibold text-black pt-3">
+              Course Description
+            </h3>
+            <p
+              className="pt-2 rich-text"
+              dangerouslySetInnerHTML={{
+                __html: courseData.courseDescription,
+              }}
+            ></p>
+          </div>
         </div>
         {/* right Column */}
-        <div></div>
+        <div className="max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
+          <img src={courseData.courseThumbnail} alt="" />
+          <div className="p-5">
+            <div className="flex items-center gap-2 ">
+              <img
+                className="w-3.5"
+                src={assets.time_left_clock_icon}
+                alt="time left clock icon"
+              />
+              <p className="text-red-500">
+                <span className="font-medium">5 days left at this price !</span>
+              </p>
+            </div>
+
+            <div className="flex gap-3 items-center pt-2">
+              <p className="text-gray-800 md:text-4xl text-2xl font-semibold">
+                {currency}
+                {(
+                  courseData.coursePrice -
+                  (courseData.discount * courseData.coursePrice) / 100
+                ).toFixed(2)}
+              </p>
+              <p className="md:text-lg text-gray-500 line-through">
+                {currency}
+                {courseData.coursePrice}
+              </p>
+              <p className="md:text-lg text-gray-500">
+                {courseData.discount}% off
+              </p>
+            </div>
+            <div className="flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500">
+              <div className="flex items-center gap-1 ">
+                <img src={assets.star} alt="star icon" />
+                <p>{calculateRating(courseData)}</p>
+              </div>
+              <div className="h-4 w-px bg-gray-500/40"></div>
+              <div className="flex items-center gap-1 ">
+                <img src={assets.time_clock_icon} alt="time clock icon" />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+              <div className="h-4 w-px bg-gray-500/40"></div>
+              <div className="flex items-center gap-1 ">
+                <img src={assets.lesson_icon} alt="time clock icon" />
+                <p>{calculateNoOfLectures(courseData)}Lessons</p>
+              </div>
+            </div>
+            <button
+              className={`px-10 mt-5  py-3 rounded-md text-white bg-blue-600 w-full ${
+                isAllreadyEnrolled ? "bg-blue-900" : ""
+              }`}
+            >
+              {isAllreadyEnrolled ? "You are Enrolled" : "Enroll Now"}
+            </button>
+            <div className="pt-6 ">
+              <p className="md:text-xl text-lg font-medium text-gray-800">
+                What's in this course ?
+              </p>
+              <ul className="ml-4 pt-2 text-sm md:text-default list-disc text-gray-500">
+                <li>Life time updates with free updates.</li>
+                <li>Step by step hands on project guidance.</li>
+                <li>Downloadable resources and source code.</li>
+                <li>Quizes and test your knowledge .</li>
+                <li>Certificate of completion.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </>
   ) : (
     <Loading />
