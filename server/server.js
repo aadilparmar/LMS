6c5c37404b676f09037728cc34ae6fded4474aef
aadilparmar <124196983+aadilparmar@users.noreply.github.com>
2025-02-15 01:31:44 +0530

    DB commit-2

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectDB from './configs/mongodb.js'
 //initalize Express
 const app =express()
 
 //Middlewares
 app.use(cors())
+//Connect to database
+await connectDB()
 
 //Routes
 app.get('/',(req,res)=>res.send("API Working"))
